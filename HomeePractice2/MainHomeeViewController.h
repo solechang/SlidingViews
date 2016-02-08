5df3327e99f4a0cc0a5970e321db2aa33be8168f
Chang <changwoo92@gmail.com>
2016-02-08 15:32:59 -0800

    commit

---
+++ HomeePractice2/MainHomeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainHomeeViewController.h
+//  HomeePractice2
+//
+//  Created by Jake Choi on 2/1/16.
+//  Copyright © 2016 solechang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainHomeeViewController : UIViewController <UIScrollViewDelegate>
+
+@end
